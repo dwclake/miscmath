49452df2d24448a40dd91b1cd97ced7e6311f654
SrebrnaMacka <devonwebb559@gmail.com>
2022-12-21 16:00:18 -0800

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+/*pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+*/
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = vector2::add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+pub mod mathlib
+{
+
+}
+
+pub mod vector2
+{
+    pub fn add(left: usize, right: usize) -> usize {
+        left + right
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use mathlib_rust;
+use mathlib_rust::{mathlib, vector2};
+
 fn main() {
     println!("Hello, world!");
-    let x: u32 = 42;
+    let mut x = 42;
     println!("X = {}.", x);
+    x = vector2::add(3,4);
+    println!("X = {}.", x);
+
+    let x: vector2;
+    x.add();
 }
