fba6eba045b9de7f4b1b2531dece7b9355eb6bb5
SrebrnaMacka <devonwebb559@gmail.com>
2022-12-28 14:07:35 -0800

    Restructured Project, added basic documentation

--- src/math/linear.rs
+++ src/math/linear.rs
@@ -11,17 +11,18 @@ mod vec2_tests {
 	}
 }
 
-
 use rand::Rng;
-//use std::cmp::Ordering;
+use std::cmp::Ordering;
+use std::ops::RangeInclusive;
 
-#[derive(Debug)]
-#[derive(PartialEq)]
+/// # Coordinate System Enum
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Coordinates {
-	Cartesian,
-	Polar,
+	CARTESIAN,
+	POLAR,
 }
 
+/// # Vec2 Struct
 #[derive(Debug)]
 pub struct Vec2{
 	pub x: f64,
@@ -32,89 +33,117 @@ pub struct Vec2{
 use Coordinates::*;
 
 impl Vec2 {
-	pub fn new( ) -> Self {
-		Self { x: 0.0, y: 0.0, system: Cartesian }
+	
+	/// # new function
+	/// Generates a new instance of Vec2 initialized to zero and returns it
+	pub fn new( ) -> Vec2 {
+		Vec2 { x: 0.0, y: 0.0, system: CARTESIAN }
 	}
 
-	pub fn create( x: f64, y: f64, system: Coordinates ) -> Self {
-		Self { x, y, system }
-	}
-
-	pub fn create_random( min: f64, max: f64, system: Coordinates ) -> Self {
-		Self {
-			x: rand::thread_rng().gen_range( min..max ),
-			y: rand::thread_rng().gen_range( min..max ),
-			system,
-		}
-	}
-
-	pub fn random_unit( min: f64, max: f64 ) -> Self {
-		let mut temp= Vec2::create( 1.0, rand::thread_rng().gen_range( min..max ), Polar );
-		temp.swap_system( Cartesian );
+	/// # create function
+	/// Generates a new instance of Vec2 initialized to chosen values and returns it
+	pub fn create( x: &f64, y: &f64, system: &Coordinates ) -> Vec2 {
+		let mut temp = Vec2 {
+			x: *x,
+			y: *y,
+			system: *system
+		};
+		temp.swap_system( CARTESIAN );
 		temp
 	}
 
-	pub fn unit( ) -> Self {
-		Self { x: 1.0, y: 1.0, system: Cartesian }
+	/// # create_random function
+	/// Generates a new instance of Vec2 initialized to random values in the range entered and returns it
+	pub fn create_random( range: &RangeInclusive<f64>, system: &Coordinates ) -> Vec2 {
+		let mut temp = Vec2 {
+			x: rand::thread_rng().gen_range( range.clone() ),
+			y: rand::thread_rng().gen_range( range.clone() ),
+			system: *system,
+		};
+		temp.swap_system( CARTESIAN );
+		temp
 	}
 
-	pub fn add( &mut self, rhs: &Vec2 ) /*-> Vec2*/ {
-		//let mut temp = Vec2::new( );
+	/// # random_unit function
+	/// Generates a new instance of Vec2 initialized to a magnitude of 1 and a angle of random value in the range entered and returns it
+	pub fn random_unit( range: &RangeInclusive<f64> ) -> Vec2 {
+		let mut temp = Vec2::create( &1.0, &rand::thread_rng().gen_range( range.clone() ), &POLAR );
+		temp.swap_system( CARTESIAN );
+		temp
+	}
+
+	/// # unit function
+	/// Generates a new instance of Vec2 initialized as a unit vector of angle 0 and returns it
+	pub fn unit( ) -> Vec2 {
+		let temp = Vec2 { x: 1.0, y: 0.0, system: POLAR };
+		temp
+	}
+	
+	/// # add function
+	/// Adds the components from the rhs Vec2 to the corresponding components of self
+	pub fn add( &mut self, rhs: &Vec2 ) -> Vec2 {
+		let mut temp = Vec2::new( );
 		self.x = self.x + rhs.x;
 		self.y = self.y + rhs.y;
-		/*temp.x = self.x;
+		temp.x = self.x;
 		temp.y = self.y;
-		temp*/
+		temp
 	}
-
+	
+	/// # dbg function
+	/// Prints debug information of self to terminal
 	pub fn dbg( &self, name: &str ) {
 		println!( "{name}: " );
 		dbg!( self );
 	}
-
+	
+	/// # div function
+	/// Divides the components of self by a scalar value rhs
 	pub fn div( &mut self, rhs: &f64 ) {
 		self.x = self.x / rhs;
 		self.y = self.y / rhs;
 	}
-
-	pub fn get_system( &self ) -> &str {
-		let system :&str = match self.system {
-			Cartesian => "Cartesian",
-			Polar => "Polar",
-		};
-		system
-	}
-
+	
+	/// # mul function
+	/// Multiplies the components of self by a scalar value rhs
 	pub fn mult( &mut self, rhs: &f64 ) {
 		self.x = self.x * rhs;
 		self.y = self.y * rhs;
 	}
-
+	
+	/// # norm function
+	/// Normalizes the magnitude of self to 1, angle is unchanged
 	pub fn norm( &mut self ) {
-		self.swap_system( Polar );
+		self.swap_system( POLAR );
 		self.x = 1.0;
-		self.swap_system( Cartesian );
+		self.swap_system( CARTESIAN );
 	}
-
+	
+	/// # rem function
+	/// Sets the components of self to the remainder of scalar division by rhs
 	pub fn rem( &mut self, rhs: &f64 ) {
 		self.x = self.x % rhs;
 		self.y = self.y % rhs;
 	}
-
+	
+	/// # sub function
+	/// Subtracts the components of the rhs Vec2 from the corresponding components of self
 	pub fn sub( &mut self, rhs: &Vec2 ) {
 		self.x = self.x - rhs.x;
 		self.y = self.y - rhs.y;
 	}
-
+	
+	/// # swap_system function
+	/// Converts components from cartesian to polar coordinates, and vice versa
 	pub fn swap_system( &mut self, new_system: Coordinates ) {
-		if self.system == Polar && new_system == Cartesian {
+		if self.system == POLAR && new_system == CARTESIAN {
 
 			let x = self.x;
 			self.x = ( (self.x).powi(2 ) + ( self.y ).powi(2 ) ).sqrt( );
 			self.y = self.y.atan2(x );
 			self.system = new_system;
 
-		} else if self.system == Cartesian && new_system == Polar {
+		} else if self.system == CARTESIAN && new_system == POLAR {
 
 			let theta = self.y;
 			self.y = self.x * self.y.sin();
