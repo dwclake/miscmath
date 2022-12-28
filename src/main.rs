3fbe73e656d0f06c3145cfdc3f7a7e429e997a71
SrebrnaMacka <devonwebb559@gmail.com>
2022-12-27 20:34:00 -0800

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
-use mathlib_rust;
-use mathlib_rust::{mathlib, vector2};
+//use mathlib_rust::*;
 
 fn main() {
-    println!("Hello, world!");
-    let mut x = 42;
-    println!("X = {}.", x);
-    x = vector2::add(3,4);
-    println!("X = {}.", x);
 
-    let x: vector2;
-    x.add();
 }
