d0cbecb9d34b9caac18baadd34e7a9d65cd80812
devonwebbR2K <devonwebb559@gmail.com>
2023-01-15 11:46:55 -0800

    Adjusted prelude to import vector but not the individual vector types

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use miscmath::prelude::*;
 
 fn main() {
-    let x = Vec2::unit();
+    let x = vector::Vec2::unit();
     dbg!(&x);
 
-    let y = Vec2::create(&0.5, &7.2);
+    let y = vector::Vec2::create(&0.5, &7.2);
     dbg!(&y);
-
+ 
     dbg!(y == y);
 }
